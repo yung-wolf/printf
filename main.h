b1cc68fd7f867c47de26d833a64ca0af8ddcac98
yung-wolf <edmundbansah@gmail.com>
2023-03-25 14:01:22 +0000

    update main.h

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 int my_printf(char *str);
 int _putchar(char c);
