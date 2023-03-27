c46e55017f644fc149b5449106236ce4a5cb5e9b
Eli <62481766+yung-wolf@users.noreply.github.com>
2023-03-27 11:22:26 +0000

    Update print.c

--- print.c
+++ print.c
@@ -1,79 +1,102 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
+#include "main.h"
 
 /**
- * _strlen - returns the len of string
- * @str: string arg
- * Return: len of str
-*/
-int _strlen(char *str)
-{
-	int i;
-
-	for (i = 0; *(str + i); i++)
-		;
-	i++;
-	return (i);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * check_buffer_overflow - if writing over buffer space,
+ * print everything then revert length back to 0 to write at buffer start
+ * @buffer: buffer holding string to print
+ * @len: position in buffer
+ * Return: length position
  */
-int _putchar(char c)
+int check_buffer_overflow(char *buffer, int len)
 {
-	return (write(1, &c, 1));
+	if (len > 1020)
+	{
+		write(1, buffer, len);
+		len = 0;
+	}
+	return (len);
 }
 
 /**
- * my_printf - prints a string to std output
- * @str: string arg
- * Return: 1 on success
-*/
-int my_printf(char *str)
-{
-	return (write(1, str, _strlen(str)));
-}
-
-/**
- * _printf - prints a string to std output
- * @format: format-string arg
- * Return: 0 on success
-*/
+ * _printf - mini printf version
+ * @format: initial string with all identifiers
+ * Return: strings with identifiers expanded
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
+	int len = 0, total_len = 0, i = 0, j = 0;
+	va_list list;
+	char *buffer, *str;
+	char* (*f)(va_list);
 
-	va_start(args, format);
-	while (*format)
+	if (format == NULL)
+		return (-1);
+
+	buffer = create_buffer();
+	if (buffer == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	while (format[i] != '\0')
 	{
-		if (*format != '%')
-		_putchar(*format);
-		else
+		if (format[i] != '%') /* copy format into buffer until '%' */
 		{
-			if (*(format + 1) == '%')
-			{
-				_putchar('%');
-				format++;
-			}
-			if (*(format + 1) == 'c')
-			{
-				_putchar(va_arg(args, int));
-				format++;
-			}
-			if (*(format + 1) == 's')
-			{
-				my_printf(va_arg(args, char*));
-				format++;
-			}
+			len = check_buffer_overflow(buffer, len);
+			buffer[len++] = format[i++];
+			total_len++;
 		}
-		format++;
+		else /* if %, find function */
+		{
+			i++;
+			if (format[i] == '\0') /* handle single ending % */
+			{
+				va_end(list);
+				free(buffer);
+				return (-1);
+			}
+			if (format[i] == '%') /* handle double %'s */
+			{
+				len = check_buffer_overflow(buffer, len);
+				buffer[len++] = format[i];
+				total_len++;
+			}
+			else
+			{
+				f = get_func(format[i]); /* grab function */
+				if (f == NULL)  /* handle fake id */
+				{
+					len = check_buffer_overflow(buffer, len);
+					buffer[len++] = '%'; total_len++;
+					buffer[len++] = format[i]; total_len++;
+				}
+				else /* return string, copy to buffer */
+				{
+					str = f(list);
+					if (str == NULL)
+					{
+						va_end(list);
+						free(buffer);
+						return (-1);
+					}
+					if (format[i] == 'c' && str[0] == '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = '\0';
+						total_len++;
+					}
+					j = 0;
+					while (str[j] != '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = str[j];
+						total_len++; j++;
+					}
+					free(str);
+				}
+			} i++;
+		}
 	}
-	va_end(args);
-
-	return (0);
+	write_buffer(buffer, len, list);
+	return (total_len);
 }
