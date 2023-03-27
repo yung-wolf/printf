e51289871e7feefe1408898443601714e904761e
Eli <62481766+yung-wolf@users.noreply.github.com>
2023-03-27 11:29:20 +0000

    Update main.h

--- main.h
+++ main.h
@@ -5,9 +5,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* helper functions */
+char* (*get_func(char i))(va_list);
+char *create_buffer(void);
+void write_buffer(char *buffer, int len, va_list list);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+/* printf functions */
 int _printf(const char *format, ...);
-int my_printf(char *str);
-int _putchar(char c);
-int _strlen(char *str);
+char *print_s(va_list list);
+char *print_c(va_list list);
+char *print_d(va_list list);
+char *itob(va_list list);
+char *rot13(va_list list);
+char *rev_string(va_list list);
+char *itoOctal(va_list list);
 
 #endif
